00b66c503ee96afe1c53e4b65ab8775d1ae2f844
Sepideh Attar <sepideh.att@gmail.com>
2022-05-20 00:26:55 +0200

    add get route but commented it ,need to work on authentication first

--- src/components/HomePage/MainFeeds.jsx
+++ src/components/HomePage/MainFeeds.jsx
@@ -1,15 +1,27 @@
 import './MainFeeds.css'
 import CreatePost from './CreatePost'
 import Post from './Post'
+import apiServices from '../../services/APIServices';
+import { useState,useEffect } from "react";
+
 
 const MainFeeds = () => {
+  const [posts, setPosts] = useState([]);
+//getting all postes in database
+//  useEffect(() => {
+//   apiServices
+//   .getPostListRoute()
+//   .then(response=>setPosts(response.data))
+  
+//   console.log(response.data)
+//  }, [])
+  
+
   return (
     <div className='Main-feeds'>
       <CreatePost  />
       <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+     
     </div>
   )
 }
