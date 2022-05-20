ba11aa56dac6703d23009804fe86fd402762dc9d
Sepideh Attar <sepideh.att@gmail.com>
2022-05-20 22:35:34 +0200

    add owner detail to comments

--- src/components/HomePage/Comment.jsx
+++ src/components/HomePage/Comment.jsx
@@ -1,8 +1,39 @@
 import './Comment.css'
+import { useState, useEffect } from 'react';
+import apiServices from '../../services/APIServices';
 
-const Comment = ({comment}) => {
+
+const Comment = ({ comment }) => {
+  const [commentOwner, setCommentOwner] = useState('')
+  const storedToken = localStorage.getItem("authToken");
+  const header = { headers: { Authorization: `Bearer ${storedToken}` } }
+
+  useEffect(() => {
+    apiServices
+      .userDetailsRoute(comment.commentOwner, header)
+      .then(response => setCommentOwner(response.data))
+      .catch(error => {
+        const errorDescription = error.response.data.errorMessage;
+        console.log("error getting user detail", errorDescription)
+        setErrorMessage(errorDescription);
+      })
+  }, [])
+
+
+
   return (
-    <div>{comment.title}</div>
+    <div className='Comment-container'>
+      <hr />
+      <div className='Comment-title-and-owner'>
+      <img src={commentOwner.profileImage || require("../../assets/placeholder.png")} className='Comment-profile-img' />
+      <span>{commentOwner.email}</span>
+        <p className='Comment-title'>{comment.title}</p>
+
+      </div>
+
+
+    </div>
+
   )
 }
 
