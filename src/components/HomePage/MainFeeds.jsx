e7adbe4eca4f324425116b66cc845de166943f8c
Sepideh Attar <sepideh.att@gmail.com>
2022-05-20 11:56:23 +0200

    get list of all posts in home page

--- src/components/HomePage/MainFeeds.jsx
+++ src/components/HomePage/MainFeeds.jsx
@@ -2,27 +2,40 @@ import './MainFeeds.css'
 import CreatePost from './CreatePost'
 import Post from './Post'
 import apiServices from '../../services/APIServices';
-import { useState,useEffect } from "react";
-
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context/auth.context"
 
 const MainFeeds = () => {
   const [posts, setPosts] = useState([]);
-//getting all postes in database
-//  useEffect(() => {
-//   apiServices
-//   .getPostListRoute()
-//   .then(response=>setPosts(response.data))
-  
-//   console.log(response.data)
-//  }, [])
-  
+  const { isLoggedIn, isLoading, user } = useContext
+
+
+  const storedToken = localStorage.getItem("authToken");
+
+  //getting all postes in database
+  useEffect(() => {
+    apiServices
+      .getPostListRoute({ headers: { Authorization: `Bearer ${storedToken}` } })
+      .then(response => setPosts(response.data))
+      .catch(e => console.log('error getting list of posts', e))
+
+  }, [])
+
 
   return (
-    <div className='Main-feeds'>
-      <CreatePost  />
-      <Post/>
-     
-    </div>
+    <>
+      {posts.length > 0
+        ? <div className='Main-feeds'>
+          <CreatePost />
+          {posts.map(post=> <Post key={post._id} post={post}  />)}
+         
+
+        </div>
+        : <p>is loading ....</p>
+    }
+
+    </>
+
   )
 }
 
