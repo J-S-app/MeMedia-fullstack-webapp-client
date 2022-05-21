74112fe9f3f1554b1eeeadd1d8de19a2d88f0406
Sepideh Attar <sepideh.att@gmail.com>
2022-05-21 14:23:03 +0200

    edit sidebar left

--- src/components/HomePage/SideBarLeft.jsx
+++ src/components/HomePage/SideBarLeft.jsx
@@ -1,29 +1,64 @@
 import "./SideBarLeft.css";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context"
+import apiServices from '../../services/APIServices';
+import { useState, useEffect, useContext } from "react";
 
 
 
-const SideBarLeft = ({ followingsList }) => {
+const SideBarLeft = () => {
+  const [followingsList, setFollowingsList] = useState([])
+  const { isLoggedIn, isLoading, user } = useContext(AuthContext);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+
+
+  const storedToken = localStorage.getItem("authToken");
+  const header = { headers: { Authorization: `Bearer ${storedToken}` } }
+
+  const callBackFollowingList = () => {
+    if (user) {
+      apiServices
+        .getFollowingsRoute(user._id, header)
+        .then(followingList => {
+          setFollowingsList(followingList.data)
+      })
+        .catch(error => {
+          const errorDescription = error.response.data.message;
+          console.log("error getting all followings", errorDescription)
+          setErrorMessage(errorDescription);
+        })
+
+    }
+  }
+  useEffect(() => {
+    callBackFollowingList()
+  }, [user?._id])
+
 
   return (
     <div className="SideBar-left">
       <div className="SideBar-left-title">
         <h4>Friends </h4>
-        {/* <NavLink to='/folowers-list'>See All</NavLink> */}
+        <NavLink to={`/${user?._id}/folowings`}>See All</NavLink>
       </div>
-
 
       {followingsList.length > 0
         ?
-        followingsList.map(following => {
-          return (
-            <>
-              <div key={following._id} className="SideBar-left-fallowers-list">
-                <img src={following.profileImage || require("../../assets/placeholder.png")} className="SideBar-left-fallowers-image" />
-                <h5 className="SideBar-left-followers-name">{following.email}</h5>
-              </div>
-            </>
-          )
+        followingsList.map((following, index) => {
+          if (index < 8) {
+            return (
+              <>
+                <div key={following._id} className="SideBar-left-fallowers-list">
+                  <img src={following.profileImage || require("../../assets/placeholder.png")} className="SideBar-left-fallowers-image" />
+                  <NavLink to={`/profile/${following._id}`}>
+                    <h5 className="SideBar-left-followers-name">{following.email}</h5>
+                  </NavLink>
+                </div>
+              </>
+            )
+          }
+
         })
         :
         ""
