052ee87b8066c950c13f67c622cc20a8b22b06ba
Sepideh Attar <sepideh.att@gmail.com>
2022-05-21 18:17:00 +0200

    add functionality for logout/needs to be more secure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,51 +3,98 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import HomeIcon from '@mui/icons-material/Home';
 import PageviewIcon from '@mui/icons-material/Pageview';
+import { useState, useEffect, useContext } from "react";                     // <== IMPORT 
+import { AuthContext } from "../../context/auth.context";
+import apiServices from '../../services/APIServices';
 
 
 const Navbar = () => {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [userDetail, setUserDetail] = useState({});
+
+  const storedToken = localStorage.getItem("authToken");
+  const header = { headers: { Authorization: `Bearer ${storedToken}` } }
+
+
+
+const CallbackGetUserDetail=()=>{
+  if( user){
+    apiServices
+    .userDetailsRoute(user._id,header)
+    .then(response=>setUserDetail(response.data) )
+    .catch(error => {
+      const errorDescription = error.response.data.message;
+      console.log("error  getting user detail", errorDescription)
+      setErrorMessage(errorDescription);
+    })
+  }
+}
+
+
+
+  useEffect(() => {
+   
+    CallbackGetUserDetail()
+  
+  }, [user?._id])
+
+
+
+
+
+
+
+
   return (
     <div className="Navbar-container">
-      <div className="Navbar-left">
-        <NavLink to='/' className="Navbar-logo">
-          MeMedia
-        </NavLink>
-        <input className ='Navbar-search-bar' placeholder='search for posts' type='search'/>
-      </div>
 
-      <div className="Navbar-center">
-      <NavLink to='/' >
-      <HomeIcon htmlColor="white" className="Navbar-center-item" />
-      </NavLink>
-{/*        
+      {isLoggedIn &&
+        <>
+          <div className="Navbar-left">
+            <NavLink to='/' className="Navbar-logo">
+              MeMedia
+            </NavLink>
+            <input className='Navbar-search-bar' placeholder='search for posts' type='search' />
+          </div>
+
+          <div className="Navbar-center">
+            <NavLink to='/' >
+              <HomeIcon htmlColor="white" className="Navbar-center-item" />
+            </NavLink>
+            {/*        
         <NavLink to='/search'>
         <PageviewIcon htmlColor="white" className="Navbar-center-item" />
         </NavLink> */}
-      </div>
-
-      <div className="Navbar-right">
-
-
-       {/* We need to add condition if user is login shows this part  */}
-{/*          
-          <button className="Nav-logoutbtn">Logout</button>
-            <NavLink to='/' className="authLink">
-              <img src={require("../../assets/profileImage/a.jpeg")} className='Navbar-profile-img' />
+          </div>
+          <div className="Navbar-right">
+            <NavLink to={`/profile/${userDetail?._id}`} className="authLink">
+              <img src={userDetail.profileImage || require("../../assets/placeholder.png")} className='Navbar-profile-img' />
             </NavLink>
-             */}
+            <button onClick={logOutUser} >Logout</button>
 
-          
+          </div>
+        </>
+      }
+
+      {!isLoggedIn &&
+        <>
+        <div className="Navbar-left">
+          <h3 style={{color : "white"}} className="Navbar-logo">MeMedia</h3>
+          </div>
+          <div className="Navbar-right">
             <NavLink to='/auth/signup' className="authLink">
               Sign up
             </NavLink>
             <NavLink to='/auth/login' className="authLink">
               Log in
             </NavLink>
-        
-      
-      </div>
-   
-     
+          </div>
+        </>
+      }
+
+
+
     </div>
   );
 };
